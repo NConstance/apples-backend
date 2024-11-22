da9015c40825a101a9fe53985d78d32d4db4396f
Constance Nwaigwe <cnwaigwe@perscholas.org>
2024-11-22 15:13:47 -0500

    corps and extra route

--- server.mjs
+++ server.mjs
@@ -2,6 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import applerouter from "./routes.mjs/appleroute.mjs";
+import cors from "cors";
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
 dotenv.config();
 const app = express();
@@ -9,8 +16,13 @@ const PORT = process.env.PORT || 3000;
 await mongoose.connect(process.env.MONGO_URI);
 
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.use("/apples", applerouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the apples api");
+});
 
 app.use((err, _req, res, next) => {
   res.status(500).send("Seems like we messed up somewhere...");
